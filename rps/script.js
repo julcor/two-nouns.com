03c6a2827255e73680a7ab0c27130f4df87880db
Julio Correa <julio.samuel.correa@gmail.com>
2022-11-30 17:39:03 -0500

    updated js

--- rps/script.js
+++ rps/script.js
@@ -10,6 +10,9 @@
 var acc_x = 0.0;
 var acc_y = 0.0;
 var acc_z = 0.0;
+var i = 0;
+var rpsSeq = ["rock-0-big.png", "rock-1-big.png", "rock-2-big.png", "rock-3-big.png", "r.png", "p.png", "s.png"];
+
 
 function getAccel(){
 	console.log("yo!");
@@ -22,9 +25,24 @@ function getAccel(){
             	acc_y = event.acceleration.y;
 				acc_z = event.acceleration.z;
                 if(acc_x < -10) {
+                	updateImage();
+                	//progress to next image
                 	document.ElementsByClassName("container").style.backgroundColor = white;
                 }
             });
         }
     });
+}
+
+
+function updateImage() {
+	if(i > 3) {
+		// Returns a random integer from 0 to 2:
+		var i = Math.floor(Math.random() * 3) + 4;
+		document.getElementById("rpsImg").src=rpsSeq[i];
+		i=0;
+	} else {
+		document.getElementById("rpsImg").src=rpsSeq[i];
+		i++;
+	}
 }
